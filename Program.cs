1139e995b23ee0a164543b42cfce7f8464c502d7
Matheus Freitas <feitosamatheus21@hotmail.com>
2022-04-04 20:11:25 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Security.Cryptography;
+using System.Reflection.Metadata.Ecma335;
+using System;
+using abstraindo_POO.src;
+using abstraindo_POO.src.entities;
+
+namespace abstraindo_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 22, TypeHero.Knight, true);
+
+            Wizard wizard = new Wizard("Jennica", 23, TypeHero.BlackWizard);
+            
+            Wizard topapa = new Wizard("Topapa", 15,TypeHero.BlackWizard);
+
+            Ninja wedge = new Ninja("Wedge", 10,TypeHero.WhiteWizard);
+
+
+            Console.Write(arus.Attack());
+
+            Console.Write($"\n Dados do jogador: {arus.ToString() + wizard.ToString() + wedge.ToString()} \n");
+
+            Console.Write($"\n Dados do jogador: {wedge.hp} \n");
+        }
+    }
+}
